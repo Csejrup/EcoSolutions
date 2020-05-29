e93138f0549b37435e01d5c321e7f8ef7943054b
CSejrup <sejrup112@gmail.com>
2020-05-29 21:48:54 +0200

    14TH Commit moved loadnewscreen (and renamed to loadpopup) to AbstractController

--- src/ecosolutions/presentation/controllers/AbstractController.java
+++ src/ecosolutions/presentation/controllers/AbstractController.java
@@ -15,18 +15,32 @@ import java.io.IOException;
 public abstract class AbstractController {
     /**
      * Method loads a new screen and assigns it to the current Stage
+     *
      * @param primaryStage
      * @param layoutfile
      */
-    protected void loadScreen(Stage primaryStage, String layoutfile){
-        try{
+    protected void loadScreen(Stage primaryStage, String layoutfile) {
+        try {
             System.out.println(layoutfile);
             Parent root = FXMLLoader.load(getClass().getResource("/ecosolutions/presentation/views/" + layoutfile));
             Scene scene = new Scene(root);
             primaryStage.setTitle("Eco Solutions");
             primaryStage.setScene(scene);
             primaryStage.show();
-        }catch(IOException e){
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void loadpopup(String layoutfile) {
+        try {
+            FXMLLoader fxmlloader = new FXMLLoader((getClass().getResource("/ecosolutions/presentation/views/" + layoutfile)));
+            Parent root2 = (Parent) fxmlloader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Overview over Active Orders");
+            stage.setScene(new Scene(root2));
+            stage.show();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
