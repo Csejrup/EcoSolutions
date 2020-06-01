c2e9cc0deb6e55739e02be7bd13f8d88332705aa
CSejrup <sejrup112@gmail.com>
2020-06-01 19:04:34 +0200

    17TH commit Piecharts added in managercontroller/view

--- src/ecosolutions/presentation/models/Status.java
+++ src/ecosolutions/presentation/models/Status.java
@@ -1,12 +1,15 @@
 package ecosolutions.presentation.models;
 
-public enum Status {
-    ;
+public enum Status {;
 
     private int CREATE = 0;
     private int UNDER_WAY = 1;
     private int CLEANING = 2;
     private int DELIVERED = 3;
     private int PAID = 4;
+
+    public static final String ORDER_DOES_NOT_EXIST = "Order does not Exist";
+    public static final String EMPLOYEE_DOES_NOT_EXIST = "Employee does not exist";
+
 
 }
