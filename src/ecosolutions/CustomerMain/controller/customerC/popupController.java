424900cc6e7acd56e0e3fd1a0dde8417a1ae3fc9
CSejrup <sejrup112@gmail.com>
2020-06-06 13:33:25 +0200

    29th commit Added CustomerViews and new DeliveryPointViews and Controllers

---
+++ src/ecosolutions/CustomerMain/controller/customerC/popupController.java
@@ -0,0 +1,17 @@
+package ecosolutions.CustomerMain.controller.customerC;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class popupController {
+
+    @FXML
+    private JFXButton btnclose;
+
+    @FXML
+    void handleClose(ActionEvent event) {
+
+    }
+
+}
---
+++ src/ecosolutions/CustomerMain/controller/deliverypoint/confirmController.java
@@ -0,0 +1,47 @@
+package ecosolutions.CustomerMain.controller.deliverypoint;
+
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class confirmController {
+
+    @FXML
+    private Button btnreturn;
+
+    @FXML
+    private JFXListView<?> itemListView;
+
+    @FXML
+    private JFXTextField numberTextField;
+
+    @FXML
+    private JFXTextField phoneTextField;
+
+    @FXML
+    private JFXTextField nameTextField;
+
+    @FXML
+    private Button btnload;
+
+    @FXML
+    private JFXTextField amountTextField;
+
+    @FXML
+    void handleConfirmation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLoadOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReturn(ActionEvent event) {
+
+    }
+
+}
