aa8b71081880a2114a3f98f872ba0f2ecf0ad1d8
CSejrup <sejrup112@gmail.com>
2020-06-11 19:11:37 +0200

    38th Commit Added Unit Testing Classes in Package "Test" Added junit_4_10.jar to the project Added junit to the project structure.

--- src/ecosolutions/presentation/controller/LoginController.java
+++ src/ecosolutions/presentation/controller/LoginController.java
@@ -14,11 +14,14 @@ import javafx.scene.input.*;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 
-public class LoginController extends AbstractController{
+public class LoginController extends AbstractController {
 
-    @FXML private JFXTextField txtUserName;
-    @FXML private JFXPasswordField txtPW;
-    @FXML private JFXButton btnLogIn;
+    @FXML
+    private JFXTextField txtUserName;
+    @FXML
+    private JFXPasswordField txtPW;
+    @FXML
+    private JFXButton btnLogIn;
     public static int accountID = 0;
 
     //Action for btnLogIn
@@ -27,50 +30,48 @@ public class LoginController extends AbstractController{
         String employeeID = txtUserName.getText();
         String password = txtPW.getText();
         // method in the comment works
+        if (employeeID.startsWith("M")) {
+            // Stage stage = (Stage) btnLogIn.getScene().getWindow();
+            // loadScreen(stage, "ManagerView.fxml");
+            verifyLogIn("ManagerView.fxml");
+        } else if (employeeID.startsWith("S")) {
+            // Stage stage = (Stage) btnLogIn.getScene().getWindow();
+            // loadScreen(stage, "deliverypointmain.fxml");
+            verifyLogIn("deliverypointmain.fxml");
+        } else if (employeeID.startsWith("L")) {
+            //  Stage stage = (Stage) btnLogIn.getScene().getWindow();
+            //  loadScreen(stage, "LaundryWorkerView.fxml");
+            verifyLogIn("LaundryWorkerView.fxml");
+        } else if (employeeID.startsWith("D")) {
+            //Stage stage = (Stage) btnLogIn.getScene().getWindow();
+            // loadScreen(stage, "DriverView.fxml");
+            verifyLogIn("DriverView.fxml");
+        }
+    }
 
-                if(employeeID.startsWith("M")) {
-                    // Stage stage = (Stage) btnLogIn.getScene().getWindow();
-                    // loadScreen(stage, "ManagerView.fxml");
-                    verifyLogIn("ManagerView.fxml");
-                }
-                else if (employeeID.startsWith("S")){
-                    // Stage stage = (Stage) btnLogIn.getScene().getWindow();
-                    // loadScreen(stage, "deliverypointmain.fxml");
-                    verifyLogIn("deliverypointmain.fxml");
-                }
-                else if (employeeID.startsWith("L")){
-                    //  Stage stage = (Stage) btnLogIn.getScene().getWindow();
-                    //  loadScreen(stage, "LaundryWorkerView.fxml");
-                    verifyLogIn("LaundryWorkerView.fxml");
-                }
-                else if (employeeID.startsWith("D")){
-                    //Stage stage = (Stage) btnLogIn.getScene().getWindow();
-                    // loadScreen(stage, "DriverView.fxml");
-                    verifyLogIn("DriverView.fxml");
-                } else{
-                    txtUserName.getStyleClass().add("wrong-credentials");
-                    txtPW.getStyleClass().add("wrong-credentials");
-                    txtUserName.setText("");
-                    txtPW.setText("");
-                }
-            }
     private void verifyLogIn(String scene) {
-        String username = txtUserName.getText();
-        String password = txtPW.getText();
-        AccountService accountService = new AccountService();
-        var account = accountService.getAccount(username);
-        String pass = account.get().getPw();
-        accountID = account.get().getAccount_id();
-        System.out.println(accountID);
-               if (account.isEmpty() || !pass.equals(password)) {
-                   showMessageDialog(null, "Wrong username or password. Try again!");
-               } else {
-                   Stage stage = (Stage) btnLogIn.getScene().getWindow();
-                   loadScreen(stage, scene);
-               }
-           }
-
-
+        try {
+            String username = txtUserName.getText();
+            String password = txtPW.getText();
+            AccountService accountService = new AccountService();
+            var account = accountService.getAccount(username);
+            String pass = account.get().getPw();
+            accountID = account.get().getAccount_id();
+            System.out.println(accountID);
+            if (account.isEmpty() || !pass.equals(password)) {
+                showMessageDialog(null, "Wrong username or password. Try again!");
+            } else {
+                Stage stage = (Stage) btnLogIn.getScene().getWindow();
+                loadScreen(stage, scene);
+            }
+        } catch (Exception e) {
+            txtUserName.getStyleClass().add("wrong-credentials");
+            txtPW.getStyleClass().add("wrong-credentials");
+            txtUserName.setText("");
+            txtPW.setText("");
+            showMessageDialog(null, "Wrong username or password. Try again!");
+        }
+    }
 }
 
 
